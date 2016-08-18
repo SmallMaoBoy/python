2716b9bdad926778fb915e82d21fad5617e3a96a
SmallMaoBoy <451345057@qq.com>
2016-08-18 17:34:13 +0800

    day 2

---
+++ day2/String.py
@@ -0,0 +1,19 @@
+print '\\'
+print '\''
+print r'\\\\'
+print '''1
+2
+3'''
+#boolean
+print 3>2 and 3<2
+print True
+print not True
+age=18
+if age >18:
+	print 'adult'
+else:
+	print 'child'	
+
+print None
+print 10/3
+print '%.2f' %(10/3.0)
